d498ae3352710b7b46e48fca565d6ff1d56e52af
Botond Sipos <Botond.Sipos@nanoporetech.com>
2020-03-19 17:54:33 +0000

    +

--- run_evaluation_dmel.sh
+++ run_evaluation_dmel.sh
@@ -9,21 +9,17 @@ OUT_DIR="evaluation/pipeline-evaluation-dmel"
 RES_DIR="$OUT_DIR/results"
 DATA_DIR="$OUT_DIR/data"
 FASTQ="$DATA_DIR/ERR3588905_1.fastq"
-REF="$DATA_DIR/Drosophila_melanogaster.BDGP6.28.dna.toplevel.fa.gz"
+REF="$DATA_DIR/Drosophila_melanogaster.BDGP6.28.dna.toplevel.fa"
 
 rm -fr $OUT_DIR/results
 mkdir -p $OUT_DIR/data
 
 if [ ! -f $REF ];
-then (cd $DATA_DIR; curl -L -C - -O $REF_URL)
+then (cd $DATA_DIR; curl -L -C - -O $REF_URL; gzip -d ${REF}.gz)
 fi
 
 if [ ! -f $FASTQ ];
 then (cd $DATA_DIR; curl -L -C - -O $FASTQ_URL; gzip -d ${FASTQ}.gz)
-fi
-
-if [ ! -f $FASTQ_FL ];
-then cdna_classifier.py -t $CORES ${FASTQ} $FASTQ_FL
 fi
 
 snakemake -j $CORES all --configfile $CONF
